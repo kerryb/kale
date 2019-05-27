9fdf9d40a18f48c87bbbdc330b573bfa055b4762
Kerry Buckley <kerryjbuckley@gmail.com>
2019-05-27 17:35:17 +0100

    Add hex info to mix file

--- mix.exs
+++ mix.exs
@@ -5,10 +5,16 @@ defmodule Kale.MixProject do
     [
       app: :kale,
       version: "0.1.0",
+      description: description(),
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
+  end
+
+  defp description do
+    "Basic given-when-then steps for ExUnit tests"
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -18,11 +24,18 @@ defmodule Kale.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Kerry Buckley"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/kerryb/kale"},
+      source_url: "https://github.com/kerryb/kale"
     ]
   end
 end
