9296607659a7ef3d16cc24f8ebf95f47a553c7b0
Kerry Buckley <kerryjbuckley@gmail.com>
2019-06-05 15:34:10 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kale.MixProject do
   def project do
     [
       app: :kale,
-      version: "0.3.0",
+      version: "0.4.0",
       description: description(),
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
