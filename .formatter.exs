f1c499c5a44194ac94eff6d841b3238907167633
Kerry Buckley <kerryjbuckley@gmail.com>
2019-06-07 09:48:43 +0100

    Export formatter rules

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,14 @@
 # Used by "mix format"
+locals_without_parens = [
+  feature: 2,
+  scenario: 2,
+  defwhen: 3,
+  defgiven: 3,
+  defwhen: 3
+]
+
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [scenario: 2]
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
